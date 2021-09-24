063648a07d02097803037d8f9f93c6159e811563
shamiik <shamikdey7@gmail.com>
2021-09-25 03:50:38 +0600

    all ui data loaded

---
+++ src/components/Product/Product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Product.css'
+const Product = (props) => {
+    // console.log(props);
+    const { name, img, seller, price, stock } = props.product;
+    return (
+        <div className='product'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className='product-name'>{name}</h4>
+                <p><small>by: {seller}</small></p>
+                <p>Price: {price}</p>
+                <p><small>only {stock} left in stock - Order soon</small></p>
+                <button
+                    onClick={() => props.handleAddToCart(props.product)}
+                    className='btn-regular'>Add to Cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
