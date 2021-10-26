f25238deeecb51abef2a204930b755e3bd37ef8e
shamiik <shamikdey7@gmail.com>
2021-10-27 02:36:57 +0600

    useproducts added

---
+++ src/components/OrderReview/OrderReview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h2>This is review</h2>
+        </div>
+    );
+};
+
+export default OrderReview;
